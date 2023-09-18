4ebadfb1351f23d72905deab792ebe2df38cf1ed
Ádisson Téjo <adissonmprtejo@gmail.com>
2023-09-18 10:59:10 -0300

    Fix out

--- src/processos.c
+++ src/processos.c
@@ -73,10 +73,6 @@ void calcula_elementos(int id_processo) {
         int linha = i / m2;
         int coluna = i % m2;
 
-        if (coluna) {
-            fprintf(arquivo, " ");
-        }
-
         int soma = 0;
 
         for (int j = 0; j < m1; j++) {
@@ -86,11 +82,7 @@ void calcula_elementos(int id_processo) {
             soma += a * b;
         }
 
-        fprintf(arquivo, "%d", soma);
-
-        if (coluna == m2 - 1) {
-            fprintf(arquivo, "\n");
-        }
+        fprintf(arquivo, "%d.%d %d\n", (linha + 1), (coluna + 1), soma);
     }
 
     shmdt(matriz1);
