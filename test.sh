a4c49da1c8babcce95f3ddd405464f71b0e4203d
Ádisson Téjo <adissonmprtejo@gmail.com>
2023-09-19 21:04:59 -0300

    Fix test script

--- test.sh
+++ test.sh
@@ -1,36 +1,32 @@
-#!/bin/bash
-
 mkdir -p tests
-
 for a in "sequencial" "processos" "threads"
 do
     > tests/$a.txt
-
-    for (( n=100; n<=800; n*=2 ))
+done
+for (( n=100; n<=800; n*=2 ))
+do
+    p=$(( $n * $n / 8))
+    if (( $n % 8 != 0 ))
+    then
+        p=$(( $p + 1 ))
+    fi
+    for a in "sequencial" "processos" "threads"
     do
-        p=$(( $n * $n / 8))
-
-        if (( $n % 8 != 0 ))
-        then
-            p=$(( $p + 1 ))
-        fi
-
         echo "Tamanho: $n x $n" >> tests/$a.txt
-
+    done
+    for i in {1..10}
+    do
         bin/gerador $n $n $n $n
-
-        for i in {1..10}
+        for a in "sequencial" "processos" "threads"
         do
             if [ $a = "sequencial" ]
             then
                 bin/$a
             else
                 bin/$a $p
-
                 bin/formatador $a
             fi
-
-            echo "  Teste $i: $(grep "segundos" output/$a/saida.txt | tr . ,)" >> tests/$a.txt
+            echo "  Matriz $i: $(grep "segundos" output/$a/saida.txt | tr . ,)" >> tests/$a.txt
         done
     done
 done
